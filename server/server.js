40822e099aed2d52c840155e21a944330f3c1506
Jeyachandran J <125851197+jeyachandranj@users.noreply.github.com>
2024-11-22 20:41:26 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -448,4 +448,4 @@ app.get("/api/dashboard", async (req, res) => {
 
 
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(443, () => console.log("Server running on port 443"));
